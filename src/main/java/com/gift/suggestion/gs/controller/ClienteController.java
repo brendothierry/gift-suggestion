ae184490ddc0747018532102b0988a9334278286
Brendo Thierry <poxabrendo@hotmail.com>
2023-12-13 20:29:06 -0300

    Adicionando novo endpoint de integração com o chat GPT

--- src/main/java/com/gift/suggestion/gs/controller/ClienteController.java
+++ src/main/java/com/gift/suggestion/gs/controller/ClienteController.java
@@ -7,7 +7,11 @@ import java.util.UUID;
 import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,22 +21,70 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
+import com.gift.suggestion.gs.DTO.ChatCompletionResponse;
 import com.gift.suggestion.gs.DTO.ClienteDTO;
 import com.gift.suggestion.gs.model.ClienteModel;
 import com.gift.suggestion.gs.service.ClienteService;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 @Controller
 @CrossOrigin(origins = "*", maxAge = 3600)
-@RequestMapping("/gift-suggestion")
-
 public class ClienteController {
 
-	final ClienteService clienteService;
+	@Value("${openai.api.url")
+	private String OPENAI_API_URL;
+	@Value("${openai.api.key")
+	private String OPENAI_API_KEY;
 
+	final ClienteService clienteService;
+	
 	public ClienteController(ClienteService clienteService) {
 		this.clienteService = clienteService;
+	}
+
+	@PostMapping("/gs/obterRespostaDoChatGPT")
+	public ResponseEntity<ChatCompletionResponse> obterRespostaDoChatGPT(@RequestBody String mensagem) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.setBearerAuth(OPENAI_API_KEY);
+
+		JsonObject requestBodyObject = new JsonObject();
+		requestBodyObject.addProperty("model", "gpt-3.5-turbo");
+
+		JsonObject systemMessage = new JsonObject();
+		systemMessage.addProperty("role", "system");
+		systemMessage.addProperty("content", "Você é um assistente virtual.");
+
+		JsonObject userMessage = new JsonObject();
+		userMessage.addProperty("role", "user");
+		userMessage.addProperty("content", mensagem);
+		requestBodyObject.add("messages", new Gson().toJsonTree(new JsonObject[] { systemMessage, userMessage }));
+
+		String requestBody = new Gson().toJson(requestBodyObject);
+		HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<ChatCompletionResponse> responseEntity = restTemplate.postForEntity(OPENAI_API_URL,
+				requestEntity, ChatCompletionResponse.class);
+
+		try {
+			if (responseEntity.getStatusCode().is2xxSuccessful()) {
+				ChatCompletionResponse response = responseEntity.getBody();
+				List<ChatCompletionResponse.Choice> choices = response.getChoices();
+
+				if (choices != null && !choices.isEmpty()) {
+					ChatCompletionResponse.Choice firstChoice = choices.get(0);
+					ChatCompletionResponse.Message message = firstChoice.getMessage();
+					System.out.println("Retorno =" + message.getContent().toString());
+					return new ResponseEntity<ChatCompletionResponse>(response, HttpStatus.OK);
+				}
+			}
+		} catch (Exception exception) {
+			exception.getMessage();
+		}
+		return null;
 	}
 
 	@PostMapping("/gs/create-cliente")
